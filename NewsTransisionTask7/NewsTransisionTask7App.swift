d9f3196b4c7a67157f6276a16698b972670f0254
Muhammad Jafar Ash Siddiq <mjafarashsiddiq@gmail.com>
2022-12-11 19:16:50 +0700

    Initial Commit

---
+++ NewsTransisionTask7/NewsTransisionTask7App.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTransisionTask7App.swift
+//  NewsTransisionTask7
+//
+//  Created by Muhammad Jafar Ash Siddiq on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsTransisionTask7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
